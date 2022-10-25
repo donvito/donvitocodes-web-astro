d7f43639a23da03fd637559e5eca8f11376e1a43
Melvin Vivas <melvindave@gmail.com>
2022-10-25 17:20:32 +0800

    customize for donvitocodes.com
    modified for donvitocodes website

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,46 +1,29 @@
 export const footerSocials = [
   {
     name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/donvito',
     icon: "mdi:twitter",
   },
   {
     name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    url: 'https://youtube.com/c/DonvitoCodes',
     icon: "mdi:youtube",
   },
   {
     name: 'Github',
-    url: 'https://github.com/littlesticks',
+    url: 'https://github.com/donvito',
     icon: "mdi:github",
   },
   {
     name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
+    url: 'https://discord.gg/udjsk9aMjE',
     icon: "ic:baseline-discord",
   },
 ]
 
-export const footerLists = [
+export const footerLists = [	
 	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
-		title: 'Company',
+		title: 'Quick Links',
 		items: [
 			{
 				title: 'About',
@@ -55,26 +38,5 @@ export const footerLists = [
 				slug: '/company/contact',
 			},
 		],
-	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
-	},
+	}
 ];
