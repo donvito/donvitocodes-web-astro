c3b495e777314227f8f4b19336096e00e989489d
Melvin Vivas <melvindave@gmail.com>
2022-10-25 19:11:38 +0800

    changed social media card

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `DonvitoCodes`,
 	description: `I livestream Tech and Startups!`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	url: `https://www.donvitocodes.com`, // No trailing slash!
 	name: `DonvitoCodes`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
